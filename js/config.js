13c4f0a9859319ec9040e87c3dfb812c774c0d85
Nokku Prudhvi <nokkuprudhvi@gmail.com>
2022-05-14 18:08:53 +0530

    working website first commit

---
+++ js/config.js
@@ -0,0 +1,12 @@
+
+var _config = {
+    cognito: {
+        userPoolId: 'eu-west-1_6Dd5KYeAL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2qjg6i4c3qccqj2tlqi8d4eqn9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1', // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://7ic2749o9g.execute-api.eu-west-1.amazonaws.com/prod', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod'
+    }
+};
+    
